f07df1d0f40df6f32c2cb2f893553b9efb90934a
Lee Hak-do <dev.fringe@gmail.com>
2020-05-15 15:40:00 +0900

    init

---
+++ view/MyFooter.ts
@@ -0,0 +1,10 @@
+import { LitElement, html, property, customElement } from 'lit-element';
+
+@customElement('my-footer')
+export class MyFooter extends LitElement {
+  render() {
+    return html`
+    <h1>footer</h1>
+    `;
+  }
+}
---
+++ view/MyHeader.ts
@@ -0,0 +1,10 @@
+import { LitElement, html, property, customElement } from 'lit-element';
+
+@customElement('my-header')
+export class MyHeader extends LitElement {
+  render() {
+    return html`
+    <h1>Header</h1>
+    `;
+  }
+}
