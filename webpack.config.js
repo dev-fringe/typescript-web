8ad8c08a59cab716c845503016ca8594eb0444ce
Lee Hak-do <dev.fringe@gmail.com>
2020-05-18 16:05:41 +0900

    init

--- webpack.config.js
+++ webpack.config.js
@@ -4,32 +4,7 @@ const createDefaultConfig = require('@open-wc/building-webpack/modern-and-legacy
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const configs = module.exports = createDefaultConfig({ //for ie11
-//  input: resolve.resolve(__dirname, '/index.html'),
-  input: resolve.resolve(__dirname, './index.ts'),
-  webpackIndexHTMLPlugin: {
-    template: ({ assets, entries, legacyEntries, variation }) => `
-    <!doctype html>
-    <html lang="ko" manifest="/cache.manifest">
-    <head>
-      <meta charset="utf-8">    
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta http-equiv="X-UA-Compatible" content="ie=edge" />      
-      <link rel="icon" href="data:;base64,iVBORw0KGgo=">      
-    </head>          
-      <body> 
-      <my-header></my-header>      
-      <header>
-        <nav>
-          <li><a href="/">Home</a></li>
-          <li><a href="/hello/world">Hello {name}</a></li>
-        </nav>
-      </header>      
-        <main></main>
-        <my-footer></my-footer>            
-      </body>
-    </html>
-    `,
-  },
+  input: resolve.resolve(__dirname, '/index.html')
 });
 
 module.exports = configs.map(config =>
