2d6a510bf9791f2dd11f0902770eb834d958c7c7
Lee Hak-do <dev.fringe@gmail.com>
2020-05-21 17:22:02 +0900

    init

--- app/view/HomeView.ts
+++ app/view/HomeView.ts
@@ -6,7 +6,7 @@ import { parser } from "@root/main"
 import { jquery } from "@root/main"
 
 const detailView = () => until(fetch('http://localhost/test').then((r) => r.text()), '<span>Loading... </span>')
-const detailView2 = async () => html`${until(fetch('http://localhost/test').then((r) => r.text()), '<span>Loading... </span>')}`;
+const detailView2 = async () => until(fetch('http://localhost/test').then((r) => r.text()), '<span>Loading... </span>');
 
 @customElement("home-view")
 class HelloView extends LitElement {
@@ -14,18 +14,33 @@ class HelloView extends LitElement {
   @property() private res: any
 
   constructor() {
-      super()     
+      super()
+      fetch('http://localhost/test')
+      .then(res => res.text())
+      .then(text => this.res = text.valueOf());         
+      // this.res = detailView2(); 
   }
 
   render() {
     const shadowRoot = this.attachShadow({mode: 'open'});
-    jquery.ajax({
-      url: 'http://localhost/test', 
-      cache: false,
-      success: function(res) {
-        shadowRoot.innerHTML = res;
-      }
-    });// have some bug 이게 최선인가?
-  //  return html`<slot name="thing"></slot>`
+    // let data = html`${detailView()}`;
+    // console.log(data.getTemplateElement());
+    // console.log(data.getHTML);
+    // console.log(data);
+    // console.log(data.getHTML());
+    // console.log(data.getHTML());
+    // console.log(data.getHTML());
+    // console.log(this.res);
+
+    shadowRoot.innerHTML = this.res;
+    
+    // jquery.ajax({
+    //   url: 'http://localhost/test', 
+    //   cache: false,
+    //   success: function(res) {
+    //     shadowRoot.innerHTML = res;
+    //   }
+    // });// have some bug 이게 최선인가?
+  //  return html`${this.res}`;
   }
 }
