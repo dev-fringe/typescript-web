0678843de5683a8090776d6f5141a573ef0984d2
Lee Hak-do <dev.fringe@gmail.com>
2020-05-21 16:25:29 +0900

    init

--- app/view/HomeView.ts
+++ app/view/HomeView.ts
@@ -1,9 +1,9 @@
 import { LitElement, customElement, html, property } from "lit-element"
-import {unsafeHTML} from 'lit-html/directives/unsafe-html.js'
-import {until} from 'lit-html/directives/until.js'
-import {asyncAppend} from 'lit-html/directives/async-append.js'
+import { unsafeHTML } from 'lit-html/directives/unsafe-html.js'
+import { until } from 'lit-html/directives/until.js'
+import { asyncAppend } from 'lit-html/directives/async-append.js'
 import { parser } from "@root/main"
-import {jquery} from "@root/main"
+import { jquery } from "@root/main"
 
 const detailView = () => until(fetch('http://localhost/test').then((r) => r.text()), '<span>Loading... </span>')
 const detailView2 = async () => html`${until(fetch('http://localhost/test').then((r) => r.text()), '<span>Loading... </span>')}`;
